5f089f3b24b3ba8914415c1fa6a2287b52ad74c7
Ryan Murphy <ryan.murphy611@gmail.com>
2023-06-15 16:09:04 -0400

    Style Change Profile pictures are better displayed

--- Carousel/script.js
+++ Carousel/script.js
@@ -1,20 +1,20 @@
 class Person {
-  constructor(name, rating, desciption) {
-    this.name = name;
-    this.rating = rating;
-    this.desciption = desciption;
-  }
+    constructor(name, rating, desciption) {
+        this.name = name;
+        this.rating = rating;
+        this.desciption = desciption;
+    }
 }
 
 let person1 = new Person(
-  "Joe",
-  2,
-  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, suscipit?"
+    "Joe",
+    2,
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, suscipit?"
 );
 let person2 = new Person(
-  "Amy",
-  5,
-  "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+    "Amy",
+    5,
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
 );
 let person3 = new Person("Mary", 4, "Lorem ipsum dolor sit amet.");
 
@@ -24,24 +24,23 @@ let personIndex = 0;
 updateContentPane(personIndex, personList[personIndex]);
 
 function changePerson(directionIndication) {
-  if (directionIndication === -1 && personIndex === 0) {
-    return;
-  }
-  if (directionIndication === 1 && personIndex === 2) {
-    return;
-  }
+    if (directionIndication === -1 && personIndex === 0) {
+        return;
+    }
+    if (directionIndication === 1 && personIndex === 2) {
+        return;
+    }
 
-  personIndex += directionIndication;
+    personIndex += directionIndication;
 
-  updateContentPane(personIndex, personList[personIndex]);
+    updateContentPane(personIndex, personList[personIndex]);
 }
 
 function updateContentPane(personIndex, currentPerson) {
-  document.getElementById("profilePicture").src =
-    "images/headshot" + (personIndex + 1) + ".png";
+    document.getElementById("profilePicture").src =
+        "images/headshot" + (personIndex + 1) + ".png";
 
-  document.getElementById("rating").src =
-    "images/" + currentPerson.rating + "star.png";
-
-  document.querySelector("p").innerHTML = currentPerson.desciption;
+    document.getElementById("rating").src =
+        "images/" + currentPerson.rating + "star.png";
+    document.querySelector("p").innerHTML = currentPerson.desciption;
 }
