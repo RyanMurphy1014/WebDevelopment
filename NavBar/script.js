887081cd37e56c69260923750951430cfb767034
RyanMurphy1014 <ryan.murphy611@gmail.com>
2023-06-28 12:56:18 -0400

    Nav bar items are functioning correctly

--- NavBar/script.js
+++ NavBar/script.js
@@ -1,30 +1,29 @@
-let activeDropDownMenu;
+let activeDropDownMenu = null;
 
 document.addEventListener("click", (e) => {
-    if (e.target === activeDropDownMenu) {
-        hideDropDown();
-        activeDropDownMenu = null;
-        return;
-    }
+  if (activeDropDownMenu != null) {
+    hideDropDown();
+  }
 
-    if (!e.target.matches("[data-dropDownButton]")) {
-        hideDropDown();
-        activeDropDownMenu = null;
-        return;
-    }
-    activeDropDownMenu = e.target;
-    showDropDown();
+  if (!e.target.matches("[data-dropDownButton]") || e.target === activeDropDownMenu) {
+    hideDropDown();
+    activeDropDownMenu = null;
     return;
+  }
+
+  activeDropDownMenu = e.target;
+  showDropDown();
+  return;
 });
 
 function showDropDown() {
-    activeDropDownMenu.nextElementSibling.style.opacity = 1;
+  activeDropDownMenu.nextElementSibling.style.opacity = 1;
 }
 
 function hideDropDown() {
-    if (activeDropDownMenu === null) {
-        return;
-    }
-    activeDropDownMenu.nextElementSibling.style.opacity = 0;
+  if (activeDropDownMenu === null) {
     return;
+  }
+  activeDropDownMenu.nextElementSibling.style.opacity = 0;
+  return;
 }
