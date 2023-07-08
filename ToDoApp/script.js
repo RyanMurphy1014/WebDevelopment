e4607d5ce7f5abd3ddf3a215a5fae8c149d56179
RyanMurphy1014 <ryan.murphy611@gmail.com>
2023-07-07 20:03:56 -0400

    Persistent Checkboxes Checkboxes remain checked/not checked when not visible.

--- ToDoApp/script.js
+++ ToDoApp/script.js
@@ -1,27 +1,31 @@
 class ToDoAppList {
-    listItems = [];
-    isListItemComplete = [];
-    listName;
+  listItems = [];
+  isListItemComplete = [];
+  listName;
 
-    addListItem(item) {
-        this.listItems.push(item);
-        this.isListItemComplete.push("F");
-    }
+  addListItem(item) {
+    this.listItems.push(item);
+    this.isListItemComplete.push("F");
+  }
 
-    completeItem(index) {
-        this.isListItemComplete[index] = "T";
-    }
+  completeItem(index) {
+    this.isListItemComplete[index] = "T";
+  }
 
-    removeListItem(index) {
-        this.listItems.splice(index, 1);
-        this.isListItemComplete.splice(index, 1);
-    }
+  uncompleteItem(index) {
+    this.isListItemComplete[index] = "F";
+  }
 
-    logListItems() {
-        this.listItems.forEach((element) => {
-            console.log(element);
-        });
-    }
+  removeListItem(index) {
+    this.listItems.splice(index, 1);
+    this.isListItemComplete.splice(index, 1);
+  }
+
+  logListItems() {
+    this.listItems.forEach((element) => {
+      console.log(element);
+    });
+  }
 }
 
 //Frequently Used Elements
@@ -47,56 +51,60 @@ listofLists.push(defaultList);
 
 //Apply Event Listeners
 listOfListDivs.forEach((element) => {
-    element.addEventListener("click", () => {
-        activeList = element;
-        clearMainContentPane();
-        displayActiveList();
-    });
+  element.addEventListener("click", () => {
+    activeList = element;
+    clearMainContentPane();
+    displayActiveList();
+  });
 });
 
 homeButton.addEventListener("click", () => {
-    activeList = null;
-    clearMainContentPane();
-    displayHome();
+  activeList = null;
+  clearMainContentPane();
+  displayHome();
 });
 
+//Functions
 function clearMainContentPane() {
-    listOfListDivs.forEach((element) => (element.style.visibility = "hidden"));
-    contentPaneText.innerHTML = null;
+  listOfListDivs.forEach((element) => (element.style.visibility = "hidden"));
+  contentPaneText.innerHTML = null;
 }
 
 function displayActiveList() {
-    let activeIndex;
-    document.querySelector("header").innerText = activeList.dataset.listname;
-    for (let i = 0; i < listofLists.length; i++) {
-        if (listofLists[i].listName === activeList.dataset.listname) {
-            activeIndex = i;
-            for (let j = 0; j < listofLists[i].listItems.length; j++) {
-                console.log(listofLists[i].isListItemComplete[j]);
-                let id = "item" + j;
-                contentPaneText.innerHTML +=
-                    "<li> <input type = 'checkbox' id = '" +
-                    id +
-                    "'> <label for=" +
-                    id +
-                    ">" +
-                    listofLists[i].listItems[j] +
-                    "</label></li>";
-            }
-        }
+  let activeIndex;
+  document.querySelector("header").innerText = activeList.dataset.listname;
+  for (let i = 0; i < listofLists.length; i++) {
+    if (listofLists[i].listName === activeList.dataset.listname) {
+      activeIndex = i;
+      for (let j = 0; j < listofLists[i].listItems.length; j++) {
+        console.log(listofLists[i].isListItemComplete[j]);
+        let id = "item" + j;
+        contentPaneText.innerHTML += "<li> <input type = 'checkbox' id = '" + id + "'> <label for=" + id + ">" + listofLists[i].listItems[j] + "</label></li>";
+      }
     }
-    checkForCompletion(listofLists[activeIndex]);
-}
+  }
 
-function checkForCompletion(list) {
-    for (let i = 0; i < list.isListItemComplete.length; i++) {
-        if (list.isListItemComplete[i] === "T") {
-            document.getElementById("item" + i).checked = true;
-        }
+  for (let i = 0; i < listofLists[activeIndex].isListItemComplete.length; i++) {
+    if (listofLists[activeIndex].isListItemComplete[i] === "T") {
+      document.querySelector("#item" + i).checked = true;
+    } else {
+      document.querySelector("#item" + i).checked = false;
     }
+  }
+
+  let checkBoxes = document.querySelectorAll("input");
+  for (let i = 0; i < checkBoxes.length; i++) {
+    checkBoxes[i].addEventListener("change", () => {
+      if (checkBoxes[i].checked === true) {
+        listofLists[activeIndex].completeItem(i);
+      } else {
+        listofLists[activeIndex].uncompleteItem(i);
+      }
+    });
+  }
 }
 
 function displayHome() {
-    listOfListDivs.forEach((element) => (element.style.visibility = "visible"));
-    headerText.innerText = "Notes";
+  listOfListDivs.forEach((element) => (element.style.visibility = "visible"));
+  headerText.innerText = "Notes";
 }
