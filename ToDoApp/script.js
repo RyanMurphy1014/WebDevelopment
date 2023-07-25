94ee348a6d4785675ca3275599de2fa16676df46
RyanMurphy1014 <ryan.murphy611@gmail.com>
2023-07-24 20:35:25 -0400

    Lists are dynamically Added

--- ToDoApp/script.js
+++ ToDoApp/script.js
@@ -1,29 +1,29 @@
 class ToDoAppList {
-    listItems = [];
-    isListItemComplete = [];
-    listName;
+  listItems = [];
+  isListItemComplete = [];
+  listName;
 
-    addListItem(item) {
-        this.listItems.push(item);
-        this.isListItemComplete.push("F");
-    }
+  addListItem(item) {
+    this.listItems.push(item);
+    this.isListItemComplete.push("F");
+  }
 
-    completeItem(index) {
-        this.isListItemComplete[index] = "T";
-    }
+  completeItem(index) {
+    this.isListItemComplete[index] = "T";
+  }
 
-    uncompleteItem(index) {
-        this.isListItemComplete[index] = "F";
-    }
+  uncompleteItem(index) {
+    this.isListItemComplete[index] = "F";
+  }
 
-    removeListItem(index) {
-        this.listItems.splice(index, 1);
-        this.isListItemComplete.splice(index, 1);
-    }
+  removeListItem(index) {
+    this.listItems.splice(index, 1);
+    this.isListItemComplete.splice(index, 1);
+  }
 }
 
 //Frequently Used Elements
-let listOfListDivs = document.querySelectorAll("main div");
+let listOfListDivs;
 let listofLists = [];
 
 const homeButton = document.querySelector("[data-homeButton]");
@@ -42,147 +42,152 @@ defaultList.addListItem("Laundry");
 defaultList.addListItem("Shopping");
 defaultList.addListItem("Cleaning");
 defaultList.listName = "Default List";
+
 //Mark item as complete
 defaultList.completeItem(0);
 listofLists.push(defaultList);
 
+//State Variables
+let hasListOfListsChanged = false;
+
+updateListHTML();
 displayHome();
 
 //Apply Event Listeners
 listOfListDivs.forEach((element) => {
-    element.addEventListener("click", () => {
-        headerText.disabled = false;
-        headerText.readOnly = false;
-        activeList = element;
-        clearMainContentPane();
-        displayActiveList();
-    });
+  console.log(listOfListDivs);
+  console.log(listofLists);
+  element.addEventListener("click", () => {
+    headerText.disabled = false;
+    headerText.readOnly = false;
+    activeList = element;
+    clearMainContentPane();
+    displayActiveList();
+  });
 });
 
 homeButton.addEventListener("click", () => {
-    activeList = null;
-    clearMainContentPane();
-    displayHome();
+  activeList = null;
+  clearMainContentPane();
+  displayHome();
 });
 
 saveIcon.addEventListener("click", () => {
-    listOfListDivs[activeIndex].dataset.listname = headerText.value;
-    listofLists[activeIndex].listName = headerText.value;
-    saveIcon.style.visibility = "hidden";
+  listOfListDivs[activeIndex].dataset.listname = headerText.value;
+  listofLists[activeIndex].listName = headerText.value;
+  saveIcon.style.visibility = "hidden";
 });
 
 headerText.addEventListener("click", () => {
-    if (activeList === null) {
-        headerText.disabled = true;
-    } else {
-        headerText.disabled = false;
-        saveIcon.style.visibility = "visible";
-    }
+  if (activeList === null) {
+    headerText.disabled = true;
+  } else {
+    headerText.disabled = false;
+    saveIcon.style.visibility = "visible";
+  }
 });
 
 headerText.addEventListener("keypress", (event) => {
-    if (event.keyCode === 13) {
-        listOfListDivs[activeIndex].dataset.listname = headerText.value;
-        listofLists[activeIndex].listName = headerText.value;
-        saveIcon.style.visibility = "hidden";
-    }
+  if (event.keyCode === 13) {
+    listOfListDivs[activeIndex].dataset.listname = headerText.value;
+    listofLists[activeIndex].listName = headerText.value;
+    saveIcon.style.visibility = "hidden";
+  }
 });
 
 //Functions
 function clearMainContentPane() {
-    listOfListDivs.forEach((element) => (element.style.visibility = "hidden"));
-    contentPaneText.innerHTML = null;
+  listOfListDivs.forEach((element) => (element.style.visibility = "hidden"));
+  document.querySelector("[data-contentPaneText]").innerHTML = null;
 }
 
 function displayActiveList() {
-    let title = activeList.dataset.listname;
-    headerText.value = title;
-    for (let i = 0; i < listofLists.length; i++) {
-        if (listofLists[i].listName === activeList.dataset.listname) {
-            activeIndex = i;
-            for (let j = 0; j < listofLists[i].listItems.length; j++) {
-                let id = "item" + j;
-                contentPaneText.innerHTML +=
-                    "<li> <input type = 'checkbox' id = '" +
-                    id +
-                    "' class = 'checkBox'> <label for=" +
-                    id +
-                    ">" +
-                    listofLists[i].listItems[j] +
-                    "</label></li>";
-            }
-        }
+  let title = activeList.dataset.listname;
+  headerText.value = title;
+  for (let i = 0; i < listofLists.length; i++) {
+    if (listofLists[i].listName === activeList.dataset.listname) {
+      activeIndex = i;
+      for (let j = 0; j < listofLists[i].listItems.length; j++) {
+        let id = "item" + j;
+        document.querySelector("[data-contentPaneText]").innerHTML += "<li> <input type = 'checkbox' id = '" + id + "' class = 'checkBox'> <label for=" + id + ">" + listofLists[i].listItems[j] + "</label></li>";
+      }
     }
-    contentPaneText.innerHTML +=
-        "<div class = 'newListItemContainer'><input class = 'newListItemTextbox' type = 'text' placeholder = 'Add new Item' ></input>" +
-        "<svg class = 'newListItemSaveButton' width='25' height='25' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>" +
-        "<path d='M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z'></path>" +
-        "<path d='M17 21v-8H7v8'></path>" +
-        "<path d='M7 3v5h8'></path>" +
-        "</svg></div>";
+  }
+  document.querySelector("[data-contentPaneText]").innerHTML +=
+    "<div class = 'newListItemContainer'><input class = 'newListItemTextbox' type = 'text' placeholder = 'Add new Item' ></input>" +
+    "<svg class = 'newListItemSaveButton' width='25' height='25' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>" +
+    "<path d='M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z'></path>" +
+    "<path d='M17 21v-8H7v8'></path>" +
+    "<path d='M7 3v5h8'></path>" +
+    "</svg></div>";
 
-    for (
-        let i = 0;
-        i < listofLists[activeIndex].isListItemComplete.length;
-        i++
-    ) {
-        if (listofLists[activeIndex].isListItemComplete[i] === "T") {
-            document.querySelector("#item" + i).checked = true;
-        } else {
-            document.querySelector("#item" + i).checked = false;
-        }
+  for (let i = 0; i < listofLists[activeIndex].isListItemComplete.length; i++) {
+    if (listofLists[activeIndex].isListItemComplete[i] === "T") {
+      document.querySelector("#item" + i).checked = true;
+    } else {
+      document.querySelector("#item" + i).checked = false;
     }
+  }
 
-    let checkBoxes = document.querySelectorAll(".checkBox");
-    for (let i = 0; i < checkBoxes.length; i++) {
-        checkBoxes[i].addEventListener("change", () => {
-            if (checkBoxes[i].checked === true) {
-                listofLists[activeIndex].completeItem(i);
-            } else {
-                listofLists[activeIndex].uncompleteItem(i);
-            }
-        });
+  let checkBoxes = document.querySelectorAll(".checkBox");
+  for (let i = 0; i < checkBoxes.length; i++) {
+    checkBoxes[i].addEventListener("change", () => {
+      if (checkBoxes[i].checked === true) {
+        listofLists[activeIndex].completeItem(i);
+      } else {
+        listofLists[activeIndex].uncompleteItem(i);
+      }
+    });
+  }
+
+  let newListItemTextbox = document.querySelector(".newListItemTextbox");
+  let newListItemSaveIcon = document.querySelector(".newListItemSaveButton");
+
+  newListItemTextbox.addEventListener("click", () => {
+    newListItemSaveIcon.style.visibility = "visible";
+  });
+
+  newListItemTextbox.addEventListener("keypress", (event) => {
+    if (event.keyCode === 13) {
+      newListItemSaveIcon.style.visibility = "";
+      listofLists[activeIndex].addListItem(newListItemTextbox.value);
+      clearMainContentPane();
+      displayActiveList();
     }
+  });
 
-    const newListItemTextbox = document.querySelector(".newListItemTextbox");
-    const newListItemSaveIcon = document.querySelector(
-        ".newListItemSaveButton"
-    );
-
-    newListItemTextbox.addEventListener("click", () => {
-        newListItemSaveIcon.style.visibility = "visible";
-    });
-
-    newListItemTextbox.addEventListener("keypress", (event) => {
-        if (event.keyCode === 13) {
-            newListItemSaveIcon.style.visibility = "";
-            listofLists[activeIndex].addListItem(newListItemTextbox.value);
-            clearMainContentPane();
-            displayActiveList();
-        }
-    });
-
-    newListItemSaveIcon.addEventListener("click", () => {
-        newListItemSaveIcon.style.visibility = "";
-        listofLists[activeIndex].addListItem(newListItemTextbox.value);
-        clearMainContentPane();
-        displayActiveList();
-    });
+  newListItemSaveIcon.addEventListener("click", () => {
+    newListItemSaveIcon.style.visibility = "";
+    listofLists[activeIndex].addListItem(newListItemTextbox.value);
+    clearMainContentPane();
+    displayActiveList();
+  });
 }
 
 function displayHome() {
-    activeList = null;
-    activeIndex = null;
+  if (hasListOfListsChanged === true) {
+    updateListHTML();
+  }
 
-    headerText.disabled = true;
+  activeList = null;
+  activeIndex = null;
 
-    listOfListDivs.forEach((element) => (element.style.visibility = "visible"));
+  headerText.disabled = true;
 
-    let thumbnailText = document.querySelectorAll("div p");
-    for (let i = 0; i < listOfListDivs.length; i++) {
-        thumbnailText[i].innerHTML = listofLists[i].listName;
-    }
+  listOfListDivs.forEach((element) => (element.style.visibility = "visible"));
 
-    headerText.value = "Notes";
-    saveIcon.style.visibility = "hidden";
+  let thumbnailText = document.querySelectorAll("div p");
+  for (let i = 0; i < listOfListDivs.length; i++) {
+    thumbnailText[i].innerHTML = listofLists[i].listName;
+  }
+
+  headerText.value = "Notes";
+  saveIcon.style.visibility = "hidden";
+}
+
+function updateListHTML() {
+  listofLists.forEach((element) => {
+    mainContentPane.innerHTML += "<div class='listThumbnail' data-listname='" + element.listName + "'><p></p></div>";
+  });
+  listOfListDivs = document.querySelectorAll("main div");
 }
